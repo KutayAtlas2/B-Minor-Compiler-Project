5c0762208660b342876e18ec3eb187e3725ce67f
Kutay <kutayyatlass@gmail.com>
2026-04-29 14:54:50 +0300

    AHHHH END ALREADY

---
+++ B-Minus Compiler/ast.c
@@ -0,0 +1,42 @@
+#include "ast.h" /* you spent an hour on this cause you forgot to include the library dumbass */
+#include <stdlib.h>
+#include <stdio.h>
+struct ast_node* make_node(ast_kind kind, struct ast_node *l, struct ast_node *r) {
+    struct ast_node *n = calloc(1, sizeof(struct ast_node));
+    n->kind = kind;
+    n->left = l;
+    n->right = r;
+    return n;
+}
+// print to ACTUALLY SEE the thing, will get disabled once assembler is complete.
+void print_ast(struct ast_node *n, int level) {
+    if (!n) return;
+
+    for (int i = 0; i < level; i++) printf("  "); 
+
+    printf("Node: %d", n->kind);
+    if (n->name) printf(" (Name: %s)", n->name);
+    if (n->kind == AST_INTEGER_LITERAL) printf(" (Val: %d)", n->int_value);
+    if (n->kind == AST_FLOAT_LITERAL) printf(" (Val: %f)", n->float_value);
+    if (n->kind == AST_TYPE && n->num_value > 0) printf("[Size: %d]", n->num_value);
+    printf("\n");
+
+    print_ast(n->left, level + 1);
+    print_ast(n->middle, level + 1);
+    print_ast(n->right, level + 1);
+    print_ast(n->next, level); // next items in a list stay at same level
+}
+//free the list to not have any memory leaks
+void free_ast(struct ast_node *n) {
+    if (!n) return;
+    
+    free_ast(n->left);
+    free_ast(n->middle);
+    free_ast(n->right);
+    free_ast(n->next);
+
+    if (n->name) free(n->name);
+    if (n->string_value) free(n->string_value);
+
+    free(n);
+}
---
+++ B-Minus Compiler/registers.h
@@ -0,0 +1,24 @@
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// use 8 general purpose registers cuz this is a simple compiler.
+#define NUM_REGS 8
+
+typedef struct {
+    const char *name;
+    bool is_free;
+    char *temp_name; // where t1, t0, etc currently lives.
+} Register;
+
+void init_registers();
+int allocate_register(char *temp);
+void free_register(int reg_index);
+void spill_all(); // yuusful at the end of blocks/functions.
+
+// helper to get the register name by index
+const char* get_reg_name(int index);
+
+#endif
